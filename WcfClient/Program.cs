85be2b7c2391f49c067e1f495b8e376811367292
cassiouru <cassio.henriqueuru@gmail.com>
2016-04-30 18:03:07 -0300

    Push Project

---
+++ WcfClient/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using WcfClient.CidadesService;
+
+namespace WcfClient
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            bool finish = false;
+
+            do
+            {
+                ObtemCidadePorEstadoClient proxy = new ObtemCidadePorEstadoClient();
+                Console.WriteLine("Informe o estado! ex: SP, RJ, PR");
+
+                foreach (string cidade in proxy.Obter(Console.ReadLine()))
+                {
+                    Console.WriteLine(cidade);
+                }
+
+                Console.ReadKey();
+                Console.WriteLine("=========================  Deseja sair? (SIM => fecha o programa) ou (NÂO => Continua)");
+
+                finish = (Console.ReadLine().Trim().ToLower() == "sim");
+
+            } while (!finish);
+        
+        }
+    }
+}
---
+++ WcfServiceSOAP/ObtemCidadePorEstado.svc.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WcfServiceSOAP
+{
+    [DataContract]
+    public class ObtemCidadePorEstado : IObtemCidadePorEstado
+    {
+        IList<Cidade> cidades;
+
+        public ObtemCidadePorEstado()
+        {
+            this.cidades = new List<Cidade>()
+            {
+                new Cidade()
+                {
+                    Nome = "URU",
+                    Uf = "SP"
+                },
+                new Cidade()
+                {
+                    Nome = "PONGAI",
+                    Uf = "SP"
+                },
+                new Cidade()
+                {
+                    Nome = "Angra dos reis",
+                    Uf = "RJ"
+                },
+                new Cidade()
+                {
+                    Nome = "Parati",
+                    Uf = "RJ"
+                },
+                new Cidade()
+                {
+                    Nome = "Rio das ostras",
+                    Uf = "RJ"
+                },
+                new Cidade()
+                {
+                    Nome = "Ponta Grossa",
+                    Uf = "PR"
+                },
+                new Cidade()
+                {
+                    Nome = "Curitiba",
+                    Uf = "PR"
+                }
+            };
+        }
+
+        public string[] Obter(string uf)
+        {
+            return this.cidades.Where(x=> x.Uf == uf.ToUpper()).Select(x=> x.Nome).ToArray();
+        }
+    }
+
+    public class Cidade
+    {
+        public string Nome { get; set; }
+        public string Uf { get; set; }
+    }
+}
